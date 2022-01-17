32a37c1defc1a5c6e2eba7e484aedc0e34f346c9
raciondecroquetas21 <96258717+raciondecroquetas21@users.noreply.github.com>
2022-01-17 20:08:57 +0100

    test conexion

--- php/array_asociativo.php
+++ php/array_asociativo.php
@@ -4,8 +4,6 @@ $alumnos_edades = array('nayra' =>18,'olaia' =>18,'jaime' => 35 );
 
 foreach ($alumnos_edades as $nombre => $edad) {
 echo 'mi nombre es '.$nombre.' y tengo '.$edad.'<br>';
-
-
 }
 
 
